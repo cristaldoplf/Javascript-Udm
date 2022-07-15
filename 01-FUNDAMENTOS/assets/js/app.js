83109bb492ee03d1fd035f4231ffb475cdad87be
cristaldoplf <cristaldoplf@gmail.com>
2022-07-15 10:32:42 -0300

    clase 14

---
+++ 01-FUNDAMENTOS/assets/js/app.js
@@ -0,0 +1,26 @@
+
+
+alert('Hola desde app.js')
+
+let 
+  a = 10, 
+  b = 20, 
+  c = 30, 
+  d = 40, 
+  x = a + b,
+  f = 'Hola',
+  g = 'Spiderman';
+
+
+  const salud = f +' '+ g;
+
+// console.log('%c Mis variables', 'color:blue; font-weight: bold')
+// console.log(a)
+// console.warn(b)
+// console.error(c)
+
+c = 'Hola de nuevo'
+
+// console.table({a,b,c,d,x,salud})
+
+var miNombre = 'pablo'
