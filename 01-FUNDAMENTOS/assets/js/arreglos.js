aea3011f2eea0bc474560c5b2837018f88261bfc
cristaldoplf <cristaldoplf@gmail.com>
2022-07-15 21:08:05 -0300

    clase 22

---
+++ 01-FUNDAMENTOS/assets/js/arreglos.js
@@ -0,0 +1,19 @@
+// const arr = new Array(10);
+// console.log(arr);
+// const arr = [];
+
+let videoJuegos = ['Mario 3', 'Megaman', 'Chrono Trigger'];
+console.log({
+  videoJuegos
+});
+
+console.log(videoJuegos[0]);
+
+let arregloCosas = [
+  true,
+  123,
+  'Pablo',
+  1 + 2,
+];
+
+console.log(arregloCosas);
---
+++ 01-FUNDAMENTOS/assets/js/palabras-reservadas.js
@@ -0,0 +1,2 @@
+// let const = 123; no podemos por que const es una palabra reservada
+
